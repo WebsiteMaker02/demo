ffe0618be3b83a4ed440ac7171e701cda032befa
WebsiteMaker02 <101118562+WebsiteMaker02@users.noreply.github.com>
2022-03-30 20:18:29 +0530

    Update settings.py

--- swastiproj/settings.py
+++ swastiproj/settings.py
@@ -132,4 +132,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-django_heroku.settings(locals())
+
