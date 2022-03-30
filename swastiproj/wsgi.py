0bfa14855892a558f6bf88e81458ab54af347d5b
WebsiteMaker02 <101118562+WebsiteMaker02@users.noreply.github.com>
2022-03-30 13:08:04 +0530

    Update wsgi.py

--- swastiproj/wsgi.py
+++ swastiproj/wsgi.py
@@ -1,6 +1,5 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xxxx.settings")
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swastiproj.settings')
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
