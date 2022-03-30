22a2c9fefe4a80f926102f8bd9ae16973b93a1cd
WebsiteMaker02 <101118562+WebsiteMaker02@users.noreply.github.com>
2022-03-30 19:56:23 +0530

    Update settings.py

--- swastiproj/settings.py
+++ swastiproj/settings.py
@@ -124,8 +124,8 @@ STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
