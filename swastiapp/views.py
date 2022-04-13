2d786cbf65a8bebbba8e741e88c5394b6a8fa21d
WebsiteMaker02 <101118562+WebsiteMaker02@users.noreply.github.com>
2022-04-13 20:39:49 +0530

    Update views.py

--- swastiapp/views.py
+++ swastiapp/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+import razorpay
 
 
 # Create your views here.
 
 def index(request):
     return render(request, "index.html")
+
+
+def payment(request):
+    if request.method == 'POST':
+        client = razorpay.Client(auth=("rzp_live_c3Tu4t0RwnyP12", "EW1Zy2lQ2T3oKwkAs3pble4I"))
+
+        DATA = {
+            "amount": 100,
+            "currency": "INR",
+            "receipt": "receipt#1",
+            "notes": {
+                "key1": "value3",
+                "key2": "value2"
+            }
+        }
+        client.order.create(data=DATA)
+    return render(request, "index1.html")
+
+
+def success(request):
+    return render(request, "success.html")
